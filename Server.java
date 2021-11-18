e41b0e2321b8d57c08a5f1b1d3fb02d3be35f148
Jibitesh Saha <z5280740@student.unsw.edu.au>
2021-11-19 05:25:49 +1100

    Added Basic Server Connection Layout

--- Server.java
+++ Server.java
@@ -1,4 +1,6 @@
 import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -7,7 +9,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.HashMap;
 
-public class Server {
+public class Server implements Runnable {
 
     private static boolean serverDebugMode;
 
@@ -17,52 +19,70 @@ public class Server {
 
     private static String credentialsFile;
     private static ArrayList<User> users;
-    
+
     private static ArrayList<HashMap<String, String>> onlineHistory;
 
+    private String threadType;
+    private Socket conn;
+
+    private String username;
+    private int userIndex;
+
     public Server() {
-        //
+        conn = null;
+        threadType = null;
+    }
+
+    public Server(Socket s) {
+        this();
+        
+        conn = s;
     }
 
     public static void main(String[] args) throws NumberFormatException, IOException {
-        
+
         if (args.length < 3) {
             System.out.println("Usage: java Server <server_port> <block_duration> <timeout>");
             return;
         }
 
-        
         if (args[0].equals("-d")) {
             if (args.length < 4) {
                 System.out.println("Usage: java Server -d <server_port> <block_duration> <timeout>");
                 return;
             }
-            
+
             serverPort = Integer.parseInt(args[1]);
             blockDuration = Integer.parseInt(args[2]);
             timeout = Integer.parseInt(args[3]);
-        
+
             serverDebugMode = true;
 
         } else {
             serverPort = Integer.parseInt(args[0]);
             blockDuration = Integer.parseInt(args[1]);
             timeout = Integer.parseInt(args[2]);
-            
+
             serverDebugMode = false;
         }
-        
+
         credentialsFile = "credentials.txt";
         users = new ArrayList<User>();
-        
+
         loadCredentialsFile();
-        
+
         onlineHistory = new ArrayList<HashMap<String, String>>();
 
-        Server mainServer = null;
-        mainServer = new Server();
+        ServerSocket mainSocket = new ServerSocket(serverPort);
+        printDebug("Server listening on port " + serverPort);
+
+        while (true) {
+            Server clientResponder = new Server(mainSocket.accept());
+            Thread thread = new Thread(clientResponder);
+        }
+
     }
-    
+
     private static void loadCredentialsFile() throws IOException {
         Path credFile = Paths.get(credentialsFile);
         List<String> credentials = Files.readAllLines(credFile);
@@ -83,6 +103,39 @@ public class Server {
             users.add(user);
         }
 
+    }
+
+    @Override
+    public void run() {
+        if (threadType == null) {
+            // TODO Read one line from the client to determine thread type
+        }
+
+        switch (threadType) {
+            case "server-read":
+                
+                break;
+        
+            case "server-write":
+                
+                break;
+
+            case "":
+                
+                break;
+
+            default:
+                break;
+        }
+
+    }
+
+    private void serverReadingThread() {
+        //
+    }
+
+    private void serverWritingThread() {
+        // user.removeOnlineOnlyJobs()
     }
 
     private static void printDebug(String msg) {
