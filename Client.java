4524b07af667c6c46ddd29c7548aa4e5573432c9
Jibitesh Saha <z5280740@student.unsw.edu.au>
2021-11-20 03:39:21 +1100

    Basic Client - Server Comms Initialisation

--- Client.java
+++ Client.java
@@ -1,4 +1,217 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
+import java.util.HashMap;
 
-public class Client {
+public class Client implements Runnable {
+
+    private static boolean clientDebugMode;
+    private static int serverPort;
+    private static String serverName;
+
+    /**
+     * Maximum number of times a thread will try to send a msg before it is deemed to have been lost
+     */
+    private static int maxSendNumber;
+
+    private static String username;
+
+    private String threadType;
+    private Socket conn;
+
+    private PrintWriter clientWriter;
+    private BufferedReader clientReader;
+    private MessageMapParser mmParser;
+
+
+    public Client() {
+        threadType = null;
+        conn = null;
+
+        clientWriter = null;
+        clientReader = null;
+        mmParser = new MessageMapParser();
+    }
+
+    public Client(String threadType) {
+        this();
+        this.threadType = threadType;
+    }
+
+    public static void main(String[] args) throws NumberFormatException {
+
+        if (args.length == 0) {
+            System.out.println("Usage: java Client <server_port>");
+            return;
+        }
+
+        if (args[0].equals("-d")) {
+            if (args.length == 1) {
+                System.out.println("Usage: java Client -d <server_port>");
+                return;
+            }
+
+            clientDebugMode = true;
+            serverPort = Integer.parseInt(args[1]);
+        } else {
+            clientDebugMode = false;
+            serverPort = Integer.parseInt(args[0]);
+        }
+
+        serverName = "localhost";
+        maxSendNumber = 10;
+
+        Client clientSender = new Client("main-write");
+        Client clientRecipient = new Client("main-read");
+
+        Thread t1 = new Thread(clientSender);
+        Thread t2 = new Thread(clientRecipient);
+
+        t1.start();
+
+    }
+
     
+    @Override
+    public void run() {
+        switch (threadType) {
+            case "main-write":
+                
+                try {
+                    clientSenderThread();
+                } catch (Exception e) {
+                    printDebug(e.getMessage());
+                    return;
+                }
+                break;
+            case "main-read":
+                
+                try {
+                    clientReaderThread();
+                } catch (Exception e) {
+                    printDebug(e.getMessage());
+                    return;
+                }
+                break;
+            case "p2p":
+                
+                break;
+
+            default:
+                return;
+
+        }
+        
+    }
+    
+    private void clientSenderThread() throws IOException, Exception {
+        
+        if (!initialiseThread()) {throw new Exception("Thread Initialisation Error");}
+
+        printDebug("Client Writer Thread Initialised");
+
+        
+        
+        
+    }
+    
+    private void clientReaderThread() throws IOException{
+        
+        
+    }
+    
+    private boolean initialiseThread() throws IOException, SocketException {
+        
+        
+        
+        if (threadType.equals("main-write") || threadType.equals("main-read")) {
+            
+            conn = new Socket(serverName, serverPort);
+            conn.setSoTimeout(100);
+            
+            clientWriter = new PrintWriter(conn.getOutputStream());
+            clientReader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+            // prepare initial message map
+
+            HashMap<String, String> initMsg = new HashMap<String, String>();
+
+            initMsg.put("tag", "thread");
+            
+            if (threadType.equals("main-write")) {
+                initMsg.put("client", "write");
+            } else {
+                initMsg.put("client", "read");
+            }
+
+            initMsg.put("body", null);
+
+            // convert to string
+
+            String initString = null;
+            try {
+                initString = mmParser.convertToMsg(initMsg);
+            } catch (Exception e) {
+                printDebug(e.getMessage());
+                return false;
+            }
+
+            HashMap<String, String> previousMap = new HashMap<String, String>();
+            for (int i = 0; i < maxSendNumber; i++) {
+                
+                printDebug("[Round " + i + "]: Sending " + initString);
+                
+                clientWriter.println(initString);
+                clientWriter.flush();
+
+
+                String recvdMsg = null;
+                try {
+                    recvdMsg = clientReader.readLine();
+                } catch (SocketTimeoutException e) {
+                    continue;
+                }
+                
+                printDebug("[Round " + i + "]: Received " + recvdMsg);
+                
+                HashMap<String, String> recvdMap;
+
+                try {
+                    recvdMap = mmParser.parseMessage(recvdMsg);
+                } catch (Exception e) {
+                    continue;
+                }
+
+                if (recvdMap.equals(previousMap)) {
+                    continue;
+                }
+
+                if (recvdMap.containsKey("status") && recvdMap.get("status").equals("recvd")) {
+                    // setup complete
+                    return true;
+                }
+
+                previousMap = recvdMap;
+
+            }
+
+            return false;
+
+
+        } // or if p2p conn...
+
+        return false;
+        
+    }
+
+    private static void printDebug(String msg) {
+        if (clientDebugMode) {
+            System.out.println(msg);
+        }
+    }
+
 }
--- Server.java
+++ Server.java
@@ -1,6 +1,11 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -16,6 +21,10 @@ public class Server implements Runnable {
     private static int serverPort;
     private static int blockDuration;
     private static int timeout;
+    /**
+     * Maximum number of times a thread will try to send a msg before it is deemed to have been lost
+     */
+    private static int maxSendNumber;
 
     private static String credentialsFile;
     private static ArrayList<User> users;
@@ -25,12 +34,23 @@ public class Server implements Runnable {
     private String threadType;
     private Socket conn;
 
+    private PrintWriter serverWriter;
+    private BufferedReader serverReader;
+    private MessageMapParser mmParser;
+
     private String username;
     private int userIndex;
 
     public Server() {
         conn = null;
         threadType = null;
+
+        username = null;
+        userIndex = -1;
+
+        serverReader = null;
+        serverWriter = null;
+        mmParser = new MessageMapParser();
     }
 
     public Server(Socket s) {
@@ -66,6 +86,8 @@ public class Server implements Runnable {
             serverDebugMode = false;
         }
 
+        maxSendNumber = 10;
+
         credentialsFile = "credentials.txt";
         users = new ArrayList<User>();
 
@@ -79,6 +101,7 @@ public class Server implements Runnable {
         while (true) {
             Server clientResponder = new Server(mainSocket.accept());
             Thread thread = new Thread(clientResponder);
+            thread.start();
         }
 
     }
@@ -107,8 +130,13 @@ public class Server implements Runnable {
 
     @Override
     public void run() {
-        if (threadType == null) {
-            // TODO Read one line from the client to determine thread type
+        if (conn != null) {
+            try {
+                if (!initialiseClientThread()) {throw new Exception("Server-Client thread not initialised");}
+            } catch (Exception e) {
+                printDebug(e.getMessage());
+                return;
+            }
         }
 
         switch (threadType) {
@@ -129,6 +157,87 @@ public class Server implements Runnable {
         }
 
     }
+
+    private boolean initialiseClientThread() throws IOException, SocketException {
+        conn.setSoTimeout(100);
+
+        serverWriter = new PrintWriter(conn.getOutputStream());
+        serverReader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+        HashMap<String, String> previousMap = new HashMap<String, String>();
+
+        for (int i = 0; i < maxSendNumber; i++) {
+            String recvdMsg = null;
+            try {
+                recvdMsg = serverReader.readLine();
+            } catch (SocketTimeoutException e) {
+                continue;
+            }
+            
+            printDebug("[Round " + i + "]: Received " + recvdMsg);
+            
+            HashMap<String, String> recvdMap;
+            
+            try {
+                recvdMap = mmParser.parseMessage(recvdMsg);
+            } catch (Exception e) {
+                continue;
+            }
+            
+            if (recvdMap.equals(previousMap)) {
+                continue;
+            }
+            
+            boolean goodMsg = false;
+
+            if (recvdMap.get("tag").equals("thread") &&
+                recvdMap.containsKey("client")) {
+                
+                String clientThreadType = recvdMap.get("client");
+
+                if (clientThreadType.equals("write")) {
+                    threadType = "server-read";
+                    goodMsg = true;
+                } else if (clientThreadType.equals("read")) {
+                    threadType = "server-write";
+                    goodMsg = true;
+                } 
+
+            }
+
+            printDebug("The thread type is: " + threadType);
+            
+            previousMap = mmParser.mapClone(recvdMap);
+
+            recvdMap.remove("client");
+
+            if (goodMsg) {
+                recvdMap.put("status", "recvd");
+            } else {
+                recvdMap.put("status", "try-again");
+            }
+
+            String returnString = null;
+            try {
+                returnString = mmParser.convertToMsg(recvdMap);
+            } catch (Exception e) {
+                printDebug(e.getMessage());
+                continue;
+            }
+
+            printDebug("[Round " + i + "]: Sending " + returnString);
+
+            serverWriter.println(returnString);
+            serverWriter.flush();
+
+            if (goodMsg) {
+                return true;
+            }
+
+        }
+
+        return false;
+    }    
 
     private void serverReadingThread() {
         //
