9f88e2daa53fb5c596746600b9376520cf8ccf45
Jibitesh Saha <z5280740@student.unsw.edu.au>
2021-11-20 05:30:10 +1100

    serverReaderThread, userAuth, indexOfUser

--- Server.java
+++ Server.java
@@ -9,6 +9,7 @@ import java.net.SocketTimeoutException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -25,6 +26,8 @@ public class Server implements Runnable {
      * Maximum number of times a thread will try to send a msg before it is deemed to have been lost
      */
     private static int maxSendNumber;
+
+    private static int maxLoginTries;
 
     private static String credentialsFile;
     private static ArrayList<User> users;
@@ -59,6 +62,18 @@ public class Server implements Runnable {
         conn = s;
     }
 
+    private static int indexOfUser(String username) {
+        if (username.equals("")) return -1;
+
+        for (int i = 0; i < users.size(); i++) {
+            if (users.get(i).getUsername().equals(username)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
     public static void main(String[] args) throws NumberFormatException, IOException {
 
         if (args.length < 3) {
@@ -87,6 +102,7 @@ public class Server implements Runnable {
         }
 
         maxSendNumber = 10;
+        maxLoginTries = 3;
 
         credentialsFile = "credentials.txt";
         users = new ArrayList<User>();
@@ -142,6 +158,8 @@ public class Server implements Runnable {
         switch (threadType) {
             case "server-read":
                 
+                serverReaderThread();
+
                 break;
         
             case "server-write":
@@ -239,11 +257,221 @@ public class Server implements Runnable {
         return false;
     }    
 
-    private void serverReadingThread() {
-        //
+    private void serverReaderThread() {
+
+        boolean userAuthed;
+        try {
+            userAuthed = userAuthAction();
+        } catch (Exception e) {
+            printDebug(e.getMessage());
+            return;
+        }
+
+        if (!userAuthed) {
+            return;
+        }
+
+
+
     }
 
-    private void serverWritingThread() {
+    private boolean userAuthAction() throws IOException {
+
+        HashMap<String, String> previousMap = new HashMap<String, String>();
+
+        String username = null;
+
+        int tryNum = 0;
+        for (int i = 0; i < maxSendNumber; i++) {
+            String recvdMsg = null;
+            try {
+                recvdMsg = serverReader.readLine();
+            } catch (SocketTimeoutException e) {
+                continue;
+            }
+            
+            printDebug("[Round " + i + "]: Received " + recvdMsg);
+            
+            HashMap<String, String> recvdMap;
+            
+            try {
+                recvdMap = mmParser.parseMessage(recvdMsg);
+            } catch (Exception e) {
+                continue;
+            }
+            
+            if (recvdMap.equals(previousMap)) {
+                continue;
+            }
+            
+            boolean goodMsg = false;
+            HashMap<String, String> responseMap;
+
+            if (!recvdMap.get("tag").equals("auth") ||
+                !recvdMap.containsKey("status")) {
+                continue;
+            }
+                
+            try {
+                responseMap = serverAuthAction(recvdMap, username, tryNum);                    
+            } catch (Exception e) {
+                printDebug(e.getMessage());
+                continue;
+            }
+
+            if (recvdMap.containsKey("username")) {
+                username = recvdMap.get("username");
+            }
+            
+            if (responseMap.get("status").equals("try-again")) {
+                tryNum++;
+            }
+
+            previousMap = mmParser.mapClone(recvdMap);
+            
+            String returnString = null;
+            try {
+                returnString = mmParser.convertToMsg(responseMap);
+            } catch (Exception e) {
+                printDebug(e.getMessage());
+                continue;
+            }
+            
+            printDebug("[Round " + i + "]: Sending " + returnString);
+            
+            serverWriter.println(returnString);
+            serverWriter.flush();
+            
+            if (responseMap.get("status").equals("matched") ||
+                responseMap.get("status").equals("registered")) {
+                return true;
+            }
+            
+            if (responseMap.get("status").equals("blocked")) {
+                Thread.sleep(1000L * blockDuration);
+
+                // Set unblocked
+
+                int userPos = indexOfUser(username);
+
+                users.get(userPos).setBlocked(false);
+
+                return false;
+            }
+        }
+    }
+
+    private HashMap<String, String> serverAuthAction(HashMap<String, String> map, String username, int tryNum) throws Exception {
+        String authStatus = map.get("status");
+
+        HashMap<String, String> responseMap = mmParser.mapClone(map);
+        int userPos = -1;
+        switch (authStatus) {
+            case "username":
+                
+                if (!map.containsKey("username")) {
+                    throw new Exception("<auth> does not contain 'username'");
+                }
+
+                String uname = map.get("username");
+
+                userPos = indexOfUser(uname);
+
+                responseMap.remove("username");
+
+                if (userPos == -1) {
+                    // new user
+                    responseMap.replace("status", "new-user");
+                    
+                } else {
+                    if (users.get(userPos).isLoggedIn()) {
+                        // already logged in
+                        responseMap.replace("status", "already-online");
+                        
+                    } else if (users.get(userPos).isBlocked()) {
+                        // user is Blocked
+                        responseMap.replace("status", "blocked");
+                        
+                    } else {
+                        // user can log in
+                        responseMap.replace("status", "enter-password");
+
+                    }
+                }
+
+
+                break;
+        
+            case "password":
+                
+                if (!map.containsKey("password")) {
+                    throw new Exception("<auth> does not contain 'password'");
+                }
+
+                userPos = indexOfUser(username);
+
+                String password = map.get("password");
+
+                responseMap.remove("password");
+
+                if (userPos == -1) {
+                    // New User's Password -> Create New User
+                    User newUser = new User(username, password);
+
+                    users.add(newUser);
+
+                    this.username = username;
+                    this.userIndex = users.size() - 1;
+
+                    responseMap.replace("status", "registered");
+
+                    // append credentials.txt
+
+                    String appendLine = System.getProperty("line.separator") + username + " " + password;
+                    Path credFile = Paths.get(credentialsFile);
+                    Files.write(credFile, appendLine.getBytes(), StandardOpenOption.APPEND, StandardOpenOption.CREATE);
+
+                } else {
+                    if (tryNum >= maxLoginTries) {
+                        responseMap.replace("status", "blocked");
+
+                    } else if (users.get(userPos).isBlocked()) {
+                        responseMap.replace("status", "blocked");
+                        
+                    } else {
+                        if (users.get(userPos).getPassword().equals(password)) {
+                            responseMap.replace("status", "matched");
+
+                            this.username = username;
+                            this.userIndex = userPos;
+
+                            users.get(userIndex).setLoggedIn(true);
+                            
+                        } else {
+                            if (tryNum < (maxLoginTries - 1)) {
+                                // more tries left
+                                responseMap.replace("status", "try-again");
+                            } else{
+                                // last mismatch - user now blocked
+                                responseMap.replace("status", "blocked");
+
+                            }
+
+                        }
+                    }
+                }
+
+
+                break;
+            
+            default:
+                break;
+        }
+
+        return responseMap;
+    }
+
+    private void serverWriterThread() {
         // user.removeOnlineOnlyJobs()
     }
 
